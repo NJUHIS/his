b13a0a53ddbe52f8120e607e63d775b54dcde4a2
161080047 <17182066>
2020-03-19 09:18:00 +0800

    init

---
+++ hisServer/src/main/java/com/njuhis/his/HisApplication.java
@@ -0,0 +1,13 @@
+package com.njuhis.his;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HisApplication.class, args);
+    }
+
+}
