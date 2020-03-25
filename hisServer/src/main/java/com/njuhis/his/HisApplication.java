93120270a740ff166eb49a6686123a9ef820423e
Paul <paulwongpang@foxmail.com>
2020-03-25 09:53:38 +0800

    跨域訪問

--- hisServer/src/main/java/com/njuhis/his/HisApplication.java
+++ hisServer/src/main/java/com/njuhis/his/HisApplication.java
@@ -2,12 +2,28 @@ package com.njuhis.his;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class HisApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HisApplication.class, args);
+    }
+    /***
+     * 跨域訪問。
+     * @return WebMvcConfigurer
+     */
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 
 }
