a6f898c7306807619a94fe34804b2243cab8dfe0
Paul <paulwongpang@foxmail.com>
2020-03-26 15:44:08 +0800

    application properties

--- hisServer/src/main/java/com/njuhis/his/databasetest/TestDataInserter.java
+++ hisServer/src/main/java/com/njuhis/his/databasetest/TestDataInserter.java
@@ -1,13 +1,9 @@
 package com.njuhis.his.databasetest;
 
 import com.njuhis.his.mapper.DepartmentMapper;
-import com.njuhis.his.model.Department;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
